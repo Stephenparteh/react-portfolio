7b806334a7002a0971102d36b4930bfefa393e31
Stephenparteh <stephenparteh@gmail.com>
2025-06-27 09:46:27 +0000

    initialcommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,13 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <main className="container-fluid text-white">
         <Home />
-        {/* <Navbar /> */}
-        {/* <Hero /> */}
       </main>
     </>
   );
